edbb15b73a76bc285b1409548f6b292e6bc86fcf
wheeeung <whee050916@gmail.com>
2023-05-13 21:18:26 +0900

    feat:: delete unused import

--- src/main/java/com/example/raisingjungsu/global/jwt/TokenProvider.java
+++ src/main/java/com/example/raisingjungsu/global/jwt/TokenProvider.java
@@ -12,8 +12,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 
